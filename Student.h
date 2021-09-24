ac41061a53627b717b429d1e143024c5e751243c
FY <zhangfeiyue2786@yundasys.com>
2021-09-24 14:06:49 +0800

    123123123

---
+++ Student.h
@@ -0,0 +1,18 @@
+//
+//  Student.h
+//  ICE-OC
+//
+//  Created by ICE on 2021/7/23.
+//
+
+#import "Persion.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Student : Persion
+//@property(nonatomic,strong) NSString *str;
+@property(nonatomic,assign) int a;
+@property(nonatomic,assign) int a1;
+@end
+
+NS_ASSUME_NONNULL_END
